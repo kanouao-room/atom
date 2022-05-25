ebdc7f4ef280390805eca779d2ed16e4aa979c35
kanouao <67881390+kanouao-room@users.noreply.github.com>
2022-05-25 23:13:13 +0900

    initial commit

---
+++ src/atom/Kaiwa.java
@@ -0,0 +1,30 @@
+package atom;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Kaiwa {
+
+	public static void main(String[] args) throws IOException {
+		// TODO 自動生成されたメソッド・スタブ
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("こんにちは。なにか話をしましょう");
+		
+		System.out.println("なにかコメントしてみてよ");
+		
+		String str = br.readLine();
+		
+		System.out.println("なるほどね、" + str + "か");
+		
+		System.out.println("きみがすこしでも元気になればいいなと思っているのはほんとだよ");
+		
+		System.out.println("ぼくはロボットだから嘘がつけないんだ。だから安心して。");
+		
+		
+		
+
+	}
+
+}
